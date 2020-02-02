6929bd9da7c77febb4c74a6231b84911bb0446ab
Daniel Żurawski <zurawski.daniel@gmail.com>
2020-02-02 20:40:22 +0000

    extra example for max-subarray

--- max-subarray.js
+++ max-subarray.js
@@ -17,14 +17,17 @@ var maxSubArraySlow = function(nums) {
 
 var maxSubArray = function(nums) {
     if (!nums.length) return 0;
-    let maxSum = nums[0];
-    let sum = nums[0];
+    let maxSum = nums[0];  // -2
+    let sum = nums[0];     // -2, 1, -2, 4, 3, 5, 6, 1, 5
     for(let i = 1; i < nums.length; i++) {
-        sum = Math.max(nums[i], nums[i] + sum)
+        sum = Math.max(nums[i], nums[i] + sum) // Math.max(4, 4 + 1), 5
         if (sum > maxSum) {
-            maxSum = sum;
+            maxSum = sum; // 1, 4, 5, 6
         }
     }
     return maxSum;
 };
 // [-2,1,-3,4,-1,2,1,-5,4],
+//    -1,-4,
+
+console.log('maxSubArray([-2,1,-3,4,-1,2,1,-5,4])', maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
